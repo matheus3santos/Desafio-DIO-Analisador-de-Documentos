ff62256930ec18e96db92ccdbd3c7d185581a390
Matinhas <matheusvictor007@gmail.com>
2024-11-16 18:49:52 -0300

    Commit inicial de implementação de projeto

---
+++ src/services/credit_card_service.py
@@ -0,0 +1,26 @@
+from azure.core.credentials import AzureKeyCredential
+from azure.ai.documentintelligence import DocumentIntelligenceClient
+from azure.ai.documentintelligence.models import AnalyzeDocumentRequest
+from utils.Config import Config
+
+def analize_credit_card(card_url):
+
+        credential = AzureKeyCredential(Config.API_KEY)
+        
+        document_Client = DocumentIntelligenceClient(Config.ENDPOINT, credential)
+        
+        card_info = document_Client.begin_analyze_document(
+            "prebuilt-creditCard", AnalyzeDocumentRequest(url_source = card_url))
+        result = card_info.result()
+            
+        for document in result.documents:
+            fields = document.get('fields', {})
+        
+            return {
+                "card_name": fields.get("CardholderName", {}).get("text"),
+                "card_number": fields.get("CardNumber", {}).get("text"),
+                "expiration_date": fields.get("ExpirationDate", {}).get("text"),
+                "bank_name": fields.get("IssuingBank", {}).get("text"),
+                
+            }
+        
